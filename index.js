b3c13d6e7ba439a2eaaf11281effa4e9788d9a8d
Avinash Ramsumair <avi@pure235games.com>
2022-11-12 17:13:40 -0500

    commit tree e069c0972d248655e725a8fbcf5a0fb78e8e49a1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(nameList, eventName) {
+    
+    const messageList = [];
+
+    for (let i = 0; i < nameList.length; i++) {
+    messageList.push(`Thank you, ${nameList[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messageList;
+}
+
+
+function countDown(startNumber){
+    let currentNumber = startNumber;
+    
+    while (currentNumber >= 0 ){
+        console.log(currentNumber)
+        currentNumber--;
+    }
+
+}
+
